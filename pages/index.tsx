1ba973de52f1227ac2375e18e1d9cbfcdf08c7ef
Felix Orinda <forinda82@gmail.com>
2022-09-05 04:51:19 +0300

    Migrate components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React from 'react'
+import { axiosQuery } from '@ui-services/api'
+import HomeFeaturedPost from '@ui-components/HomeFeaturedPost'
+import HomeNews from '@ui-components/HomeNews'
 
 export default function Home() {
+  const [posts, setPosts] = React.useState<any[]>([])
+
+  
+  const fetchPosts = async () => {
+    const response = await axiosQuery.get('')
+    setPosts(response.data)
+    console.log(response.data);
+  }
+  React.useEffect(() => {
+    fetchPosts()
+  } , [])
   return (
-   <h1 className='text-purple-600'>Hello from loans.co.ke</h1>
-  )
+    <div>
+      <HomeFeaturedPost posts={posts!} />
+      <HomeNews  posts={posts}/>
+    </div>
+  );
 }
