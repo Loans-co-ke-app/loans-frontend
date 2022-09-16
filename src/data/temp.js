209e0873dde463c091d35176179788ffc1a0594d
Felix Orinda <forinda82@gmail.com>
2022-09-16 17:05:57 +0300

    Add reference links

---
+++ src/data/temp.js
@@ -0,0 +1,3 @@
+const dev_link='https://www.themelooks.us/demo/usnews/html/world-news.html'
+const dev_link2='https://themes.themegoods.com/grandnews/demo1'
+const dev_link3='https://preview.themeforest.net/item/gmag-blog-news-magazine-wordpress-theme/full_screen_preview/38793728?_ga=2.190513571.906905146.1661278646-1530294902.1661278646'
