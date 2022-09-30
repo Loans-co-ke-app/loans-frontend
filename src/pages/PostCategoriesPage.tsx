aa830fb3f5348587ee73e58af881f1239ddafb2e
Felix Orinda <forinda82@gmail.com>
2022-09-30 11:37:43 +0300

    Add page load transition with framer motion

--- src/pages/PostCategoriesPage.tsx
+++ src/pages/PostCategoriesPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import nav, { INavRoute } from "../data/nav";
+import { motion } from "framer-motion";
 
 const PostCategoriesPage = () => {
   const { category } = useParams();
@@ -17,13 +18,19 @@ const PostCategoriesPage = () => {
   }, []);
 
   return (
-    <div className="min-h-screen">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 3 }}
+      className="min-h-screen"
+    >
       <div>
         {categories.map((item) => {
           return <div key={item.name}>{item.name}</div>;
         })}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
