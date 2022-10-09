75f6fc74d92f99d5531eb84e94ce5a4932d5c960
Felix Orinda <forinda82@gmail.com>
2022-10-09 06:59:19 +0300

    Update sector filter

---
+++ src/helpers/trimRegex.ts
@@ -0,0 +1,13 @@
+class TrimRegex {
+
+	public static trimRegex = (str: string, regex: RegExp, char: string) => {
+		return str.replace(regex, char);
+	};
+
+	public static trimAndReplace = (str: string, regex: RegExp, char: string) => {
+		return str.trim().replace(regex, char);
+	};
+
+}
+
+export default  TrimRegex
--- src/pages/SectorsPage.tsx
+++ src/pages/SectorsPage.tsx
@@ -1,14 +1,21 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import React from 'react';
+import { IPostEntity } from '../interfaces/Post';
 import { Link } from 'react-router-dom';
+import { PostsContext } from '../state/providers/PostsProvider';
+import React from 'react';
+import trimRegex from '../helpers/trimRegex';
 import { useParams } from 'react-router-dom';
 import { INavRoute, navLinks } from '../data/nav';
 
 const SectorsPage = () => {
+	const { state: { postsState: { posts: unfilteredPosts } } } = React.useContext(PostsContext);
+	const [filteredPosts, setFilteredPosts] = React.useState<IPostEntity[]>([]);
 	const params = useParams();
 	const [subSectors, setSubSectors] = React.useState<INavRoute[]>([]);
 	const [sector, setSector] = React.useState<string>();
 	const [sectors, setSectors] = React.useState<INavRoute[]>([]);
+	const [subSectorNames, setSubSectorNames] = React.useState<string[]>([]);
 	React.useEffect(() => {
 		return setSector(params.sector!);
 
@@ -21,16 +28,36 @@ const SectorsPage = () => {
 	React.useEffect(() => {
 		const sect = sectors.filter((sector) => sector.name.toLowerCase() === params.sector?.toLowerCase())[0];
 		const children = sect?.children ? sect.children : [];
-
-		return setSubSectors(children);
+		const ssChildren = subSectors.map((sub) => sub.name.toLowerCase());
+		setSubSectors(children);
+		setSubSectorNames(ssChildren);
 	}, [sectors, sector]);
+
+	React.useEffect(() => {
+		const filterPostBySector = (posts: IPostEntity[], sector: string, subSector: string | undefined) => {
+			const subRegex = new RegExp(/[\s_]/gm);
+console.log('------------------------------------------------');
+
+			unfilteredPosts.forEach((post) => {
+				console.log({ p: trimRegex.trimAndReplace(post.article_category!.category_name!.toLowerCase(), subRegex, '-').toLowerCase(), s: params.subSector ? params.subSector.toLowerCase() : '' });
+			}
+			);
+			// console.log("Posts",ps);
+
+		};
+
+
+		filterPostBySector(unfilteredPosts, sector!, params.subSector);
+
+	}, [sector, params.subSector, subSectors]);
+
 
 	return <div className='min-h-screen'>
 		<div className='hidden md:block py-1 px-4 bg-gray-300'>
 			<div className='flex gap-4 font-bold capitalize overflow-x-scroll no-scrollbar'>
 				<div className='text-purple-600 font-bold text-xl'>{sector}</div>
 				{subSectors.map((s) => <div key={s.name}>
-					<Link to={s.path} className={`hover:bg-gray-300 px-4 cursor-pointer w-fit text-justify text-sm whitespace-nowrap ${s.name ? s.name.toLowerCase().trim().replace(/\s/g,'-') === params.subSector?.toLowerCase() && 'border-b border-black' : 'text-yellow-600'}`}>{s.name}</Link>
+					<Link to={s.path} className={`hover:bg-gray-300 px-4 cursor-pointer w-fit text-justify text-sm whitespace-nowrap ${s.name ? s.name.toLowerCase().trim().replace(/\s/g, '-') === params.subSector?.toLowerCase() && 'border-b border-black' : 'text-yellow-600'}`}>{s.name}</Link>
 				</div>)}
 			</div>
 		</div>
